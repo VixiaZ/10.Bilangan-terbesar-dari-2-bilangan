10dc921a0cb0de8aea854ff419b4b33ae5416759
Zulfikar Hidayatullah <45659177+VixiaZ@users.noreply.github.com>
2019-01-03 01:13:01 +0700

    Add files via upload

---
+++ Bilangan terbesar dari 2 bilangan.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std ;
+
+int main ()
+	{
+		int a,b;
+		
+		cout <<"---Perogram mengetahui bilangan terbesar dari 2 bilangan---"<<endl;
+		
+		cout <<"\nMasukan bilangan Pertama = ";
+		cin >>a;
+		
+		cout <<"\nMasukan bilangan Kedua = ";
+		cin >>b;
+		
+		// Perogram Mengetahui bilangan terbesar by zulfikar
+		
+		if (a > b)
+			cout <<"bilangan terbesar dari 2 bilangan tersebut adalah = "<<a;
+		else
+			cout <<"bilangan terbesar dari 2 bilangan tersebut adalah = "<<b;
+		
+	}
